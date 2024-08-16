d8267c9be83cd05c1500689b3447aff15fa256a3
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-16 08:50:07 -0600

    Clientes estandar y vip

---
+++ Evaluacion Parcial Parte 2/ClientePremium.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_Parcial_Parte_2
+{
+    internal class ClientePremium : Clientes
+    {
+        public ClientePremium(string nombre, string correo, string direccion) : base(nombre, correo, direccion)
+        {
+        }
+    }
+}
