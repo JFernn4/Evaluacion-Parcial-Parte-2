e2c26054d388d51e1eb485efd5816b16ce71a9f3
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-16 08:48:44 -0600

    Clase de Clientes

---
+++ Evaluacion Parcial Parte 2/Clientes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_Parcial_Parte_2
+{
+    internal class Clientes
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; } 
+        public string Direccion { get; set; }
+
+        public Clientes(string nombre, string correo, string direccion)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Direccion = direccion;
+        }
+    }
+}
