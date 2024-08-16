e64bca57762462b04219521ca72519c906b7a257
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-16 08:51:13 -0600

    Clase de pedidos

---
+++ Evaluacion Parcial Parte 2/Pedidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_Parcial_Parte_2
+{
+    internal class Pedidos
+    {
+        public int Numero { get; set; }
+        public string Fecha { get; set; }
+        public Clientes Clientes { get; set; }
+    }
+}
